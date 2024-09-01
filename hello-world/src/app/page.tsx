790137dc73097343f0b2cfc95fe6d2800137d1ab
Bushra Attique <152737575+bushra223@users.noreply.github.com>
2024-09-01 12:25:40 +0500

    changed name

--- hello-world/src/app/page.tsx
+++ hello-world/src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>My name is Bushra Attique and this is my first NextJs project.</div>
+    <div>
+      My name is Bushra Attique Ahmed and this is my first NextJs project.
+    </div>
   );
 }
