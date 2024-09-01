452fe55497a3693a17f046af19ee5cfe2281ba3a
Bushra Attique <152737575+bushra223@users.noreply.github.com>
2024-09-01 16:03:33 +0500

    updated

---
+++ hello-world-manual-deployment/my-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      Hi! My name is Bushra Attique and this is my first project which i am
+      going to deploy manually.
+    </div>
+  );
+}
