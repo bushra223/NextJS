83762df19cde4ae4717c3ca37eea9fe709f621d2
Bushra Attique <152737575+bushra223@users.noreply.github.com>
2024-09-18 21:41:16 +0500

    error removed

--- tic-tac-toe/src/app/square.tsx
+++ tic-tac-toe/src/app/square.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-function Square({ value, onSquareClick }) {
+function Square({
+  value,
+  onSquareClick,
+}: {
+  value: string;
+  onSquareClick: () => void;
+}) {
   return (
-    // <button
-    //   className="bg-blue-500 hover:opacity-50 text-lime-200 font-bold py-2 rounded min-w-[60px] min-h-[60px] text-4xl relative"
-    //   onClick={onSquareClick}
-    // >
-    //   {value}
-    //   <div className=" top-0 left-0 border-b-4 min-w-[60px] min-h-[60px] border-yellow-500 transform -rotate-45 origin-top-right"></div>
-    // </button>
     <button
       className="bg-blue-500 hover:opacity-50 text-lime-200 font-bold py-2 rounded min-w-[60px] min-h-[60px] text-4xl "
       onClick={onSquareClick}
